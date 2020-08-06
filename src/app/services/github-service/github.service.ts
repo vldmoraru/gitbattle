0c5f30e4a1dd098f91c11088449a35b109263372
Vlad Moraru <56203442+vldmoraru@users.noreply.github.com>
2020-08-06 16:33:34 +0300

    Update github.service.ts

--- src/app/services/github-service/github.service.ts
+++ src/app/services/github-service/github.service.ts
@@ -1,6 +1,6 @@
 import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
-import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
    providedIn: 'root',
